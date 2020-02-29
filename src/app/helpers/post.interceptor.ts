e73f5e028da3b3b89a43756cdb7fb17c91ac4587
Paige <paigechoi0701@gmail.com>
2020-02-29 18:42:33 +0900

    add voting function

---
+++ src/app/helpers/post.interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {CookieService} from 'ngx-cookie-service';
+
+@Injectable()
+export class PostInterceptor implements HttpInterceptor {
+
+  constructor(private cookieSerive: CookieService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.info('request ===== ', request)
+    if (request.method.toUpperCase() == 'POST') {
+      let csrf = this.cookieSerive.get('csrftoken');
+      let sessionId = this.cookieSerive.get('sessionid');
+      request = request.clone({
+        setHeaders: {
+          'X-CSRFToken': csrf,
+          'credentials': 'include',
+          'mode': 'same-origin',
+        }
+      });
+    }
+    return next.handle(request);
+  }
+}
