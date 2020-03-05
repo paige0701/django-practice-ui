7731b7a49ceddb910e35f931b9a5bc49b50083b5
Paige <paigechoi0701@gmail.com>
2020-03-05 17:56:36 +0900

    add bootstrap, nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,21 @@ import {AppRoutingModule} from './app.routing.module';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {CookieService} from 'ngx-cookie-service';
 import {PostInterceptor} from './helpers/post.interceptor';
+import {FooterComponent} from './common/footer/footer.component';
+import {HeaderComponent} from './common/header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     AppRoutingModule,
     HttpClientModule,
     BrowserModule,
+
   ],
   providers:
     [
