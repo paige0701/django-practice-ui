84a7bfb01abe65df25adcfbe862bb01ddcd4c3e3
Paige <paigechoi0701@gmail.com>
2020-03-12 16:23:02 +0900

    - show vocabularies by category - create category service

--- src/app/language/category/category.component.ts
+++ src/app/language/category/category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {LanguageService} from '../language.service';
 
 @Component({
   selector: 'app-category',
@@ -8,41 +9,20 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class CategoryComponent implements OnInit {
   data: {
-    'id': number, 'name': string, 'words' : {'id': string, 'eng': string, 'kor': string, 'esp': string}[]
+    'id': number, 'name': string, 'words' : {'id': number, 'eng': string, 'kor': string, 'esp': string}[]
   };
-  randomItem: {'id': string, 'eng': string, 'kor': string, 'esp': string};
-  constructor(private activatedRoute: ActivatedRoute) {
+  randomItem: {'id': number, 'eng': string, 'kor': string, 'esp': string};
+  constructor(private activatedRoute: ActivatedRoute,
+              private langService: LanguageService) {
     this.activatedRoute.params.subscribe((result) => {
-      switch (result['id'].toString()) {
-        case '1':
-          this.showNumbers();
-          break;
-        case '2':
-          this.showMonths();
-          break;
-
-      }
+      this.langService.getWordsByCategory(result['id']).subscribe((result) => {
+        this.data = result;
+        this.onClickNext();
+      })
     })
   }
 
   ngOnInit(): void {
-  }
-
-  showNumbers() {
-    console.info('numbers')
-  }
-
-  showMonths() {
-    this.data = {
-      'id': 2,
-      'name': 'Months',
-      'words': [
-        {'id': 'mo-1', 'eng': 'January', 'kor': '1월', 'esp': 'enero'},
-        {'id': 'mo-2', 'eng': 'February', 'kor': '2월', 'esp': 'febrero'},
-        {'id': 'mo-2', 'eng': 'March', 'kor': '3월', 'esp': 'marzo'}
-      ]
-    }
-    this.randomItem = this.data.words[this.getRandomNumber(0, this.data.words.length)]
   }
 
   getRandomNumber(min, max) {
@@ -51,7 +31,7 @@ export class CategoryComponent implements OnInit {
 
   onClickNext() {
     const temp = this.data.words[this.getRandomNumber(0, this.data.words.length)];
-    if (this.randomItem.eng === temp.eng) {
+    if (this.randomItem && this.randomItem.eng === temp.eng) {
       this.onClickNext()
     } else {
       this.randomItem = temp
--- src/app/language/language.service.ts
+++ src/app/language/language.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/index';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguageService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWordsByCategory(catetory: number): Observable<any> {
+    return this.http.get(`${environment.host}/lang/${catetory}/`) as Observable<any>
+  }
+
 }
